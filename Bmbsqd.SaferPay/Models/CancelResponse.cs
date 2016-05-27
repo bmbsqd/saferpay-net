d68c02e534ffb7ba0d47fb5344f70bff6fc5abc3
Daniel Durante <me@danieldurante.com>
2016-05-27 15:45:19 -0400

    Changes DateTime to DateTimeOffset.

--- Bmbsqd.SaferPay/Models/CancelResponse.cs
+++ Bmbsqd.SaferPay/Models/CancelResponse.cs
@@ -6,6 +6,6 @@ namespace Bmbsqd.SaferPay.Models
 	{
 		public string TransactionId { get; set; }
 		public string OrderId { get; set; }
-		public DateTime Date { get; set; }
+		public DateTimeOffset Date { get; set; }
 	}
 }
